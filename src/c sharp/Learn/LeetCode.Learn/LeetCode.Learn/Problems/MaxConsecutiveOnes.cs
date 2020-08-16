7821ca527db43eea93d81c49fec89e0ad8207ab5
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-17 02:41:56 +0530

    Solution for Max Consecutive Ones problem statement

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Problems/MaxConsecutiveOnes.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.Learn.Arrays101.Problems
+{
+    //Problem references : https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3238/
+    class MaxConsecutiveOnes
+    {
+        public int FindMaxConsecutiveOnes(int[] numbers)
+        {
+            int maximumConsecutiveLength = 0;
+            int length = numbers.Length;
+
+            //If numbers is null
+            if (numbers is null)
+                return 0;
+
+            int onesCount = 0;
+            for (int iterator = 0; iterator < length; iterator++)
+            {
+                if (numbers[iterator] == 1)
+                {
+                    onesCount++;
+                }
+                else
+                {
+                    if (onesCount > maximumConsecutiveLength)
+                        maximumConsecutiveLength = onesCount;
+                    onesCount = 0;
+                }
+            }
+
+            if (onesCount > maximumConsecutiveLength)
+                return onesCount;
+
+            return maximumConsecutiveLength;
+        }
+    }
+}
--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Program.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using LeetCode.Learn.Arrays101.Problems;
+using System;
 
 namespace LeetCode.Learn.Arrays101
 {
+    //This is main entry point to test all solution
+    //uncomment required code to run the particular problem ( only one main method should be uncommet at a time)
     class Program
     {
+        //Default Main method
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Hello World!");
+        //}
+
+        //1. Max Consecutive Ones
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
+
+            // Test Case 1
+            var items = new int[] { 1, 1, 0, 1, 1, 1 };
+            //Expected result : 3
+            var result1 = maxConsecutiveOnes.FindMaxConsecutiveOnes(items);
+
+            //Test Case 2
         }
     }
 }
