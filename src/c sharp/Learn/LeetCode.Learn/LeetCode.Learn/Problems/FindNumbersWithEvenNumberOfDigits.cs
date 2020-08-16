606471747f954ffdaff0887629daf5b8980c2fcc
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-17 03:11:39 +0530

    Learn -> Array101 -> Find Numbers with Even Number of Digits solution

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Problems/FindNumbersWithEvenNumberOfDigits.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode.Learn.Arrays101.Problems
+{
+    //Problem statement : https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
+    class FindNumbersWithEvenNumberOfDigits
+    {
+        //Method 1 : Use the build ToString() method and find the lenght
+        //public int FindNumbers(int[] nums)
+        //{
+        //    int numberOfEvenDigitsNumbers = 0;
+        //    int length = nums.Length;
+
+        //    for (int iterator = 0; iterator < length; iterator++)
+        //    {
+        //        if (nums[iterator].ToString().Length % 2 == 0)
+        //            numberOfEvenDigitsNumbers++;
+        //    }
+
+        //    return numberOfEvenDigitsNumbers;
+        //}
+
+        //Method 2 : Write the method to calculate the digits of numbers and find out the even of add
+        // Run : 90.3% better Memory :
+        public int FindNumbers(int[] nums)
+        {
+            int numberOfEvenDigitsNumbers = 0;
+            int length = nums.Length;
+
+            for (int iterator = 0; iterator < length; iterator++)
+            {
+                if (GetNumberOfDigits(nums[iterator]) % 2 == 0)
+                    numberOfEvenDigitsNumbers++;
+            }
+
+            return numberOfEvenDigitsNumbers;
+        }
+
+        public int GetNumberOfDigits(int number)
+        {
+            int result = 0;
+            while (number > 0)
+            {
+                result++;
+                number = number / 10;
+            }
+
+            return result;
+        }
+    }
+}
--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Program.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn/Program.cs
@@ -8,13 +8,21 @@ namespace LeetCode.Learn.Arrays101
     class Program
     {
         //Default Main method
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Hello World!");
-        //}
-
-        //1. Max Consecutive Ones
+        //Call required Main method inside default Main method.
+        //Example : FindNumbersWithEvenNumberOfDigits_Main() to execute the solution for
+        //Find Numbers with Even Number of Digits
         static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //1.Max Consecutive Ones
+            //MaxConsecutiveOnes_Main(args);
+
+            //2. Find Numbers with Even Number of Digits    
+            FindNumbersWithEvenNumberOfDigits_Main(args);
+        }
+
+        //1. Max Consecutive Ones       
+        static void MaxConsecutiveOnes_Main(string[] args)
         {
             MaxConsecutiveOnes maxConsecutiveOnes = new MaxConsecutiveOnes();
 
@@ -22,6 +30,19 @@ namespace LeetCode.Learn.Arrays101
             var items = new int[] { 1, 1, 0, 1, 1, 1 };
             //Expected result : 3
             var result1 = maxConsecutiveOnes.FindMaxConsecutiveOnes(items);
+
+            //Test Case 2
+        }
+
+        //2. Find Numbers with Even Number of Digits    
+        static void FindNumbersWithEvenNumberOfDigits_Main(string[] args)
+        {
+            FindNumbersWithEvenNumberOfDigits findNumbersWithEvenNumberOfDigits = new FindNumbersWithEvenNumberOfDigits();
+
+            // Test Case 1
+            var items = new int[] { 12, 345, 2, 6, 7896 };
+            //Expected result : 2
+            var result1 = findNumbersWithEvenNumberOfDigits.FindNumbers(items);
 
             //Test Case 2
         }
