fee7d9a12c7b79677d9f9a9862fe3d15dbe9d9a9
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-18 03:08:31 +0530

    Learn->Array101 -> Solution for  MergeSortedArray problem statement

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/MergeSortedArray.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Learn.Arrays101.Problems
+{
+    class MergeSortedArray
+    {
+        //Method 1
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            if (m > 0 && n > 0)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    int temp = nums2[0];
+                    int j = 0;
+                    if (nums1[i] > nums2[j])
+                    {
+                        nums2[j++] = nums1[i];
+
+                        while (j < n && nums2[j] <= nums1[i])
+                        {
+                            int temp1 = nums2[j];
+                            nums2[j] = nums2[j - 1];
+                            nums2[j - 1] = temp1;
+                            j++;
+                        }
+
+                        nums1[i] = temp;
+                    }
+                }
+            }
+
+            if (m > 0)
+            {
+                for (int i = m, j = 0; i < (n + m) && j < n; i++, j++)
+                {
+                    nums1[i] = nums2[j];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    nums1[i] = nums2[i];
+                }
+            }
+        }
+    }
+}
