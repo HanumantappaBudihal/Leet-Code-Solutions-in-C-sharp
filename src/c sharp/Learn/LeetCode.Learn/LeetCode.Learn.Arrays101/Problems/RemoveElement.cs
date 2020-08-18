ab717e78f299856415f453c4a1f7f15e384464d4
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-19 00:07:24 +0530

    Improved the Learn->Array101-> RemoveElements problelm statement

--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/RemoveElement.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/RemoveElement.cs
@@ -7,8 +7,54 @@ namespace LeetCode.Learn.Arrays101.Problems
     //Problem statement :  https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
     class RemoveElementProblem
     {
-        //Method 1 :
+        //Method2 : Better performance
         public int RemoveElement(int[] nums, int val)
+        {
+            int n = nums.Length;
+            int i, j, x;
+            for (i = 0; i < n; i++)
+            {
+                if (nums[i] == val)
+                {
+                    for (j = n - 1; j > i; j--)
+                    {
+                        if (nums[j] != val) break;
+                    }
+                    if (i == j) break;
+                    else
+                    {
+                        int temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+            }
+            return i;
+        }
+
+        public int RemoveElement_Method2(int[] nums, int val)
+        {
+            int i = 0;
+            int n = nums.Length;
+            while (i < n)
+            {
+                if (nums[i] == val)
+                {
+                    nums[i] = nums[n - 1];
+                    // reduce array size by one
+                    n--;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return n;
+        }
+
+        //Method 1 : Even mainted the order here ( excluding the item from list, but maintained the order)
+        public int RemoveElement_Method1(int[] nums, int val)
         {
             if (nums is null)
                 return 0;
