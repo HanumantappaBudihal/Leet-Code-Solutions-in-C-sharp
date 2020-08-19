1cf152a02db2c899435679cf61cd8bf97e061bda
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-20 03:33:33 +0530

    Update the RemoveDuplicatesFromSortedArray solution with better approach

--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/RemoveDuplicatesFromSortedArray.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/RemoveDuplicatesFromSortedArray.cs
@@ -2,31 +2,56 @@
 {
     class RemoveDuplicatesFromSortedArray
     {
-        public int RemoveDuplicates(int[] nums)
+        //Method 1 :
+        public int RemoveDuplicates(int[] numbers)
         {
-            if (nums is null || nums.Length <= 0)
+            if (numbers is null || numbers.Length <= 0)
                 return 0;
 
-            int duplicateItem = nums[0];
+            int duplicateItem = numbers[0];
             int newLength = 1;
-            int length = nums.Length;
+            int length = numbers.Length;
 
             for (int i = 1; i < (length); i++)
             {
-                while(true)
+                while (true)
                 {
-                    if (duplicateItem != nums[i])
+                    if (duplicateItem != numbers[i])
                     {
-                        nums[newLength++] = nums[i];
-                        duplicateItem = nums[i];
+                        numbers[newLength++] = numbers[i];
+                        duplicateItem = numbers[i];
                         break;
                     }
 
                     i++;
-                }                
+                }
             }
 
             return newLength;
+        }
+
+        //Method 2 :
+        public int RemoveDuplicates_Method2(int[] numbers)
+        {
+            int length = numbers.Length;
+            if (length == 0) return 0;
+
+            int count = 0;
+            for (int i = 1; i < length; i++)
+            {
+                if (numbers[i] != numbers[count])
+                {
+                    count++;
+                    numbers[count] = numbers[i];
+                }
+            }
+
+            for (int i = count + 1; i < length; i++)
+            {
+                numbers[i] = 0;
+            }
+
+            return count + 1;
         }
     }
 }
