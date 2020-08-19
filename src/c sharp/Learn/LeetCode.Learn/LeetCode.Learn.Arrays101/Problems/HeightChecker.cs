49473857ad940dd0bd361265ee88330a6f02907e
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-20 05:17:11 +0530

    Learn -> Array101 -> Height Cheker solution

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Arrays101/Problems/HeightChecker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Learn.Arrays101.Problems
+{
+    class HeightCheckerProblem
+    {
+        public int HeightChecker(int[] heights)
+        {
+            int lenght = heights.Length;
+            var newArray = heights.ToList();
+            newArray.Sort();
+            int swapCount = 0;
+            for(int i=0; i < lenght;i++)
+            {
+                if (heights[i] != newArray[i])
+                    swapCount++;
+            }
+
+            return swapCount;
+        }
+    }
+}
