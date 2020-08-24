010f9d879339b170fc95de43e4e10f6e6828fb5c
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-25 03:07:14 +0530

    Learn-> Linked list : listed down the premium problem statements : TODO Need to solve later

--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.LinkedList/Problems/CopyListWithRandomPointer.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.LinkedList/Problems/CopyListWithRandomPointer.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace LeetCode.Learn.LinkedList.Problems
+﻿namespace LeetCode.Learn.LinkedList.Problems
 {
     class CopyListWithRandomPointer
     {
+        //TODO Need to implement : Need to Subscription to see the problem
     }
 }
---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.LinkedList/Problems/DesignDoublyLinkedList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Learn.LinkedList.Problems
+{
+    class DesignDoublyLinkedList
+    {
+        //TODO Need to implement : Need to Subscription to see the problem
+    }
+}
