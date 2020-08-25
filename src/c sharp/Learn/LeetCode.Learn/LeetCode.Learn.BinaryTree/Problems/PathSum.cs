d63fa7067b5cc3049c0dca03def7ba8211119974
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-25 07:26:11 +0530

    Corrected the compiler error in Symmetric tree code

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.BinaryTree/Problems/PathSum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Learn.BinaryTree.Problems
+{
+    //Problem statement : https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/537/
+    class PathSum
+    {
+        public bool HasPathSum(TreeNode root, int sum)
+        {
+
+            return false;
+        }
+    }
+}
