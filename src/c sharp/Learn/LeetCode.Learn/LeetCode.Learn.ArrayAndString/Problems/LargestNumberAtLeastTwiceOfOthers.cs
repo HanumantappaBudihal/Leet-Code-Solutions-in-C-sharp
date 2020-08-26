80f590779bdcc75e9f6848fdbab01f15c3eefd83
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-27 04:38:29 +0530

    Learn : Array and Stack : Largest Number At Least Twice Of Others

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.ArrayAndString/Problems/LargestNumberAtLeastTwiceOfOthers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Learn.ArrayAndString.Problems
+{
+    //Problem statement :https://leetcode.com/explore/learn/card/array-and-string/201/introduction-to-array/1147/
+    // Solution : https://leetcode.com/submissions/detail/386852249/
+    class LargestNumberAtLeastTwiceOfOthers
+    {
+        public int DominantIndex(int[] numbers)
+        {
+            //First iteration , find out the biggest number
+
+            int maxIndex = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[maxIndex] < numbers[i])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == maxIndex)
+                    continue;
+
+                if (numbers[maxIndex] < (2 * numbers[i]))
+                    return -1;
+            }
+
+            return maxIndex;
+        }
+    }
+}
--- src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.ArrayAndString/Program.cs
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.ArrayAndString/Program.cs
@@ -11,7 +11,32 @@ namespace LeetCode.Learn.ArrayAndString
             Console.WriteLine("Hello World!");
 
             //1.FindPivotIndex
-            FindPivotIndex_Main();
+            //FindPivotIndex_Main();
+
+            //2.LargestNumberAtLeastTwiceOfOthers
+            LargestNumberAtLeastTwiceOfOthers_Main();
+        }
+
+        private static void LargestNumberAtLeastTwiceOfOthers_Main()
+        {
+            LargestNumberAtLeastTwiceOfOthers largestNumberAtLeastTwiceOfOthers = new LargestNumberAtLeastTwiceOfOthers();
+
+            //Test case 1
+            //Input: nums = [3, 6, 1, 0]
+            //Output: 1
+
+            var numbers = new int[] { 3, 6, 1, 0 };
+            var result = largestNumberAtLeastTwiceOfOthers.DominantIndex(numbers);
+
+            //Test case 2
+            /*Input: nums = [1, 2, 3, 4]
+            Output: -1*/
+
+            numbers = new int[] { 1, 2, 3 };
+            result = largestNumberAtLeastTwiceOfOthers.DominantIndex(numbers);
+
+            //Test case 
+            result = largestNumberAtLeastTwiceOfOthers.DominantIndex(new int[] { 1 });
         }
 
         private static void FindPivotIndex_Main()
