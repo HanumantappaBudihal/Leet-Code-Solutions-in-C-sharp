c344228fe0d020022b710434561dbaba6af595ce
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-28 22:50:00 +0530

    Learn : Test case test project seupt for all project

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.Recusion2.TestCases/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LeetCode.Learn.Recusion2.TestCases
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
