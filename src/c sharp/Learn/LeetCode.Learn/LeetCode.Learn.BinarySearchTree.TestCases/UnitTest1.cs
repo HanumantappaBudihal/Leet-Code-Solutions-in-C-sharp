f661bdbabd332fa5450367a8c450e1f7fa5a57e0
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-28 22:43:33 +0530

    Leanrn : Binary Search Tree : Test cases project setup

---
+++ src/c sharp/Learn/LeetCode.Learn/LeetCode.Learn.BinarySearchTree.TestCases/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LeetCode.Learn.BinarySearchTree.TestCases
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
