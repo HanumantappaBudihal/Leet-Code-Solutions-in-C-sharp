f1a24e59e74ed8219388aff881737c0e443e7992
Hanumantappa Budihal <hanumanth.k.budihal@gmail.com>
2020-08-30 08:06:48 +0530

    Added the Weekly_Contest_204 project to solution

---
+++ src/c sharp/Challeges/LeetCode.WeeklyContest/LeetCode.WeeklyContest.204/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetCode.WeeklyContest._204
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
